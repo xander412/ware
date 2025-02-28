af7c5a12338322e59320b1fcd1cfc449a903b561
Sivaprasad Chintha <sivaprasadc412@gmail.com>
2025-02-28 15:31:20 +0530

    Initial Commit

---
+++ server.py
@@ -0,0 +1,43 @@
+
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Respond to GET requests
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        print("Send Something Man : ", end = '')
+        self.wfile.write(input().encode())
+
+    def do_POST(self):
+        # Handle POST request and print the data received
+        content_length = int(self.headers['Content-Length'])  # Get the size of the data
+        post_data = self.rfile.read(content_length)  # Read the data sent in the POST request
+
+        # Print the received data (for debugging)
+        print("Received POST data:", post_data.decode('utf-8'))
+
+        # Send a JSON response back to the client
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        response = {
+            "status": "success",
+            "received_data": post_data.decode('utf-8')
+        }
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
+
+# Set up the server to run on port 8000
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run(port=8000)
+
